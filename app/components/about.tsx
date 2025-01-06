bd5358d214ebe4c47ce6a2948cc28d63a114eea3
JuniorsQ <quintanajuniors@gmail.com>
2025-01-06 01:23:16 -0400

    small corrections in the ortography

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -8,14 +8,15 @@ export default function About(){
     <div className="sectionHeader"><h1>Acerca</h1></div>
     <div className="aboutSectionContent">
         <h1 className="mb-0">
-            Quintana
+            Quintana 
+            <hr />
             <span className="text-primary">Juniors</span>
         </h1>
         <div className="subheading mb-5">
-            Caracas Venezuela · (+58) 412-311-9581 ·
-            <a href="mailto:name@email.com"> quintanajuniors@email.com</a>
+            Caracas Venezuela · (+58) 412-211-9581 ·
+            <h3> <a href="mailto:name@email.com"> quintanajuniors@email.com</a>   </h3> 
         </div>
-        <p className="lead mb-5">Desarrollador fullstack con 7 años de experiencia, actualmente me especializo en el desarrollo de aplicaciones para Terminales de Pago (POS) en diversos lenguajes de programación C++, C, Dart, manejo de la mensajería ISO8583 , criptografía de datos.Tambien manejo otras tecnologias e implementación de soluciones para el sector bancario.</p>
+        <p className="lead mb-5">Desarrollador fullstack con más de 5 años de experiencia, actualmente me dedico al desarrollo de aplicaciones para Terminales de Pago (POS) en diversos lenguajes de programación C++, java, flutter, dart. Conocimiento de la mensajería ISO8583, criptografía de datos (DES, DUKPT, 3DES). También poseo conocimiento de otras tecnologías e implementación de soluciones para el sector bancario. Experiencia comprobable en certificaciones de mensajería ISO8583 y certificaciones MTIP, amplios conocimientos en los estándares de pagos EMV (Europay, Mastercard y Visa).</p>
         <div className="social-icons">
             <a className="social-icon" href="https://www.linkedin.com/in/juniors-quintana-11a90a182/"> <FontAwesomeIcon className="list-inline-item" icon={['fab', 'linkedin']} /> </a>
             <a className="social-icon" href="https://github.com/juniorsQ"> <FontAwesomeIcon className="list-inline-item" icon={['fab', 'github']} />   </a>
