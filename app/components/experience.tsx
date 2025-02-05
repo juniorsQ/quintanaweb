030e6110d24150427e3546447f01e4ed259c40eb
JuniorsQ <quintanajuniors@gmail.com>
2025-02-05 00:34:30 -0400

    three

--- app/components/experience.tsx
+++ app/components/experience.tsx
@@ -9,7 +9,14 @@ export  default function Experience(){
             <div className="flex-grow-1">
                 <h3 className="mb-0">Desarrollador fullstack - Inteligensa S.A</h3>
                 <div className="subheading mb-3">Inteligensa</div>
-                <p>Desarrollador Fullstack y Líder de Proyecto Mi rol principal es innovar y desarrollar aplicaciones de pago para terminales POS. Tengo amplia experiencia en el desarrollo de aplicaciones de pago para terminales Android, con certificaciones MTIP aprobadas y aplicaciones actualmente en producción. También he desarrollado e implementado DLL's para impresoras de tarjetas bancarias. Además, ofrezco soporte y asesoría a sistemas de control de gestiones, desarrollos web, aplicaciones móviles y otros proyectos consolidados en la empresa. Soy autodidacta y me apasionan los nuevos avances en programación y manejo de datos para crear soluciones que simplifiquen la vida cotidiana.</p>
+                <p>Desarrollador fullstack y líder de proyecto en el
+                    área de sistemas. Mi tarea primordial en la organización es innovar, crear y optimizar
+                    los aplicativos en terminales de pago (POS), para la fecha estoy ejecutando el desarrollo de un aplicativo
+                    para terminales Android SO. Además de dar soporte/asesoría a sistemas de control de gestiones, desarrollos
+                    web, aplicaciones móviles entre otros proyectos ya afianzados en la empresa. Me considero una persona
+                    autodidacta e interesado en los nuevos avances en cuanto a programación y manejo de datos para
+                    desarrollar soluciones que faciliten los procesos de la vida cotidiana
+                </p>
             </div>
             <div className="flex-shrink-0"><span className="text-primary">Enero 2017 - Presente</span></div>
         </div>
